0f989363834c17fbf4c8e4a6833679593fa46ab8
rshezarr <rshezarr@gmail.com>
2023-05-04 01:30:58 +0600

    update: producer

--- producer/producer.go
+++ producer/producer.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
+	"time"
 
+	"github.com/segmentio/kafka-go"
+)
+
+func main() {
+	conn, err := kafka.DialLeader(context.Background(), "tcp", "localhost:9092", "topic_test", 0)
+	if err != nil {
+		log.Fatalf("Error while dialing: %v", err)
+	}
+
+	conn.SetWriteDeadline(time.Now().Add(time.Second * 10))
+
+	conn.WriteMessages(kafka.Message{Value: []byte("Hello")})
 }
